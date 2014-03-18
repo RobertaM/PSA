f933171f66a9c3248a2ab40b7103db9597614b32
MartynasMac <martynas.maciul@gmail.com>
2014-03-18 17:00:49 +0200

    Url and Breadcrumb now load automatically

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -67,7 +67,6 @@ $autoload['libraries'] = array('database', 'session');
  */
 
 $autoload['helper'] = array('url', 'breadcrumb');
-//$autoload['helper'] = array();
 
 /*
   | -------------------------------------------------------------------
