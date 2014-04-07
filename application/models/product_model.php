0f57a1a715017d4f24c7ca06d0d77e04551121ad
nedas <nedas@nedas-300V5Z>
2014-04-07 10:45:53 +0300

    SUTVARKYTA DB

--- application/models/product_model.php
+++ application/models/product_model.php
@@ -18,13 +18,13 @@ class Product_model extends CI_Model {
         $place_id = $place["id"];
 
         // Select all data
-        $this->db->select("items.*, PLACES.place_id, categories.*,ITEM_OPTIONS.*");
+        $this->db->select("ITEMS.*, PLACES.place_id, CATEGORIES.*,ITEM_OPTIONS.*");
         $this->db->from("PLACES");
         $this->db->join("PLACE_ITEMS", "PLACE_ITEMS.place_id = PLACES.place_id", "left");
-        $this->db->join("items", "items.item_id = PLACE_ITEMS.item_id", "left");
-        $this->db->join("categories", "categories.cat_id = items.cat_id", "left");
-        $this->db->join("ITEM_OPTIONS", "ITEM_OPTIONS.item_id = items.item_id", "left");
-        $this->db->order_by('items.item_type asc, ITEM_OPTIONS.item_id asc');
+        $this->db->join("ITEMS", "ITEMS.item_id = PLACE_ITEMS.item_id", "left");
+        $this->db->join("CATEGORIES", "CATEGORIES.cat_id = ITEMS.cat_id", "left");
+        $this->db->join("ITEM_OPTIONS", "ITEM_OPTIONS.item_id = ITEMS.item_id", "left");
+        $this->db->order_by('ITEMS.item_type asc, ITEM_OPTIONS.item_id asc');
         $this->db->where("PLACES.place_id = " . $place_id);
         $response = $this->db->get()->result_array();
 
