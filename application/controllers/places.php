2201c021f64b5e0b2c9b0061108aeaa05b89d3dd
martynasmaciulevicius <visiskaj.slaptaj@gmail.com>
2014-04-30 09:27:55 +0300

    Places fix

--- application/controllers/places.php
+++ application/controllers/places.php
@@ -51,23 +51,23 @@ class Places extends CI_Controller {
             );
 
             $this->load->view("static/footer");
-            exit();
+        } else {
+
+            // Get selected radio button index from session if it exists
+            $post_array = $this->input->post(null, TRUE);
+            $selected_button = $post_array["place-radio"];
+
+            // Load all the submitted data into session variable
+            $this->Place_model->temporarily_save_selected_place(
+                    array(
+                        "id" => $selected_button,
+                        "name" => $places[$selected_button]["name"]
+                    )
+            );
+
+            // Redirect
+            redirect(base_url("products/select"), 'refresh');
         }
-
-        // Get selected radio button index from session if it exists
-        $post_array = $this->input->post(null, TRUE);
-        $selected_button = $post_array["place-radio"];
-
-        // Load all the submitted data into session variable
-        $this->Place_model->temporarily_save_selected_place(
-                array(
-                    "id" => $selected_button,
-                    "name" => $places[$selected_button]["name"]
-                )
-        );
-
-        // Redirect
-        redirect(base_url("products/select"), 'refresh');
     }
 
 }
