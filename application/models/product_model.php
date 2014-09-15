d1538afa454ee2a47e3a9bdd841d785da7cd723a
Le M <lem@linuxmint.linuxmint-domain>
2014-09-15 22:10:58 +0300

    Not working product request added

--- application/models/product_model.php
+++ application/models/product_model.php
@@ -5,17 +5,20 @@ class Product_model extends CI_Model {
     /**
      * @return place list.
      */
-    public function get_products() {
+    public function get_products($place_id = NULL) {
 
         // Get selected place from Place_model
         $this->load->model("Place_model");
         $place = $this->Place_model->get_selected_place();
 
         // Goodbye if no place returned
-        if (!isset($place["id"]))
-            return FALSE;
-
-        $place_id = $place["id"];
+        if (!isset($place_id)) {
+            if (!isset($place["id"]))
+                return FALSE;
+            else {
+                $place_id = $place["id"];
+            }
+        }
 
         // Select all data
         $this->db->select("ITEMS.*, PLACES.place_id, CATEGORIES.*,ITEM_OPTIONS.*");
@@ -30,8 +33,9 @@ class Product_model extends CI_Model {
 
         return $response;
     }
-    public function get_categories(){
-        
+
+    public function get_categories() {
+
         $this->db->select('CATEGORIES.cat_id,CATEGORIES.cat_name');
         $this->db->from('CATEGORIES');
         $response = $this->db->get()->result_array();
@@ -59,64 +63,66 @@ class Product_model extends CI_Model {
 
     public function set_product() {
 
-        if ($this->input->post('new_category') == NULL){
-        
-        $item = array(
-            'item_name' => $this->input->post('item_name'),
-            'image' => $this->input->post('item_image'),
-            'cat_id' => $this->input->post('cat')
-                );
-        $this->db->insert("ITEMS", $item); } 
-        
-        if ($this->input->post('new_category') != NULL){
-            
-            $new_cat = array( 
-            'cat_name' => $this->input->post('new_category'));
+        if ($this->input->post('new_category') == NULL) {
+
+            $item = array(
+                'item_name' => $this->input->post('item_name'),
+                'image' => $this->input->post('item_image'),
+                'cat_id' => $this->input->post('cat')
+            );
+            $this->db->insert("ITEMS", $item);
+        }
+
+        if ($this->input->post('new_category') != NULL) {
+
+            $new_cat = array(
+                'cat_name' => $this->input->post('new_category'));
             $this->db->insert("CATEGORIES", $new_cat);
             $new_id = $this->db->insert_id();
-            
+
             $item = array(
-            'item_name' => $this->input->post('item_name'),
-            'image' => $this->input->post('item_image'),
-            'cat_id' => $new_id
-                );
-         $this->db->insert("ITEMS", $item);
+                'item_name' => $this->input->post('item_name'),
+                'image' => $this->input->post('item_image'),
+                'cat_id' => $new_id
+            );
+            $this->db->insert("ITEMS", $item);
         }
-         $id = $this->db->insert_id();
-         
+        $id = $this->db->insert_id();
+
         $place_id = $this->session->userdata("user_data[place_id]");
-        
-         $place_item = array(
+
+        $place_item = array(
             'item_id' => $id,
             'place_id' => $place_id,
             'is_available' => ("1")
-                );
-         $this->db->insert("PLACE_ITEMS", $place_item);
-         
-         $item_option1 = array(
+        );
+        $this->db->insert("PLACE_ITEMS", $place_item);
+
+        $item_option1 = array(
             'item_id' => $id,
             'option_name' => $this->input->post('option_name1'),
             'price' => $this->input->post('price1')
-                );
-         $this->db->insert("ITEM_OPTIONS", $item_option1);
-         
-         if ($this->input->post('option_name2') != NULL) {
-         
-         $item_option2 = array(
-            'item_id' => $id,
-            'option_name' => $this->input->post('option_name2'),
-            'price' => $this->input->post('price2')
-                );
-         $this->db->insert("ITEM_OPTIONS", $item_option2);}
-         
-         if ($this->input->post('option_name3') != NULL){
-         $item_option3 = array(
-            'item_id' => $id,
-            'option_name' => $this->input->post('option_name3'),
-            'price' => $this->input->post('price3')
-                );
-         $this->db->insert("ITEM_OPTIONS", $item_option3);}
+        );
+        $this->db->insert("ITEM_OPTIONS", $item_option1);
 
+        if ($this->input->post('option_name2') != NULL) {
+
+            $item_option2 = array(
+                'item_id' => $id,
+                'option_name' => $this->input->post('option_name2'),
+                'price' => $this->input->post('price2')
+            );
+            $this->db->insert("ITEM_OPTIONS", $item_option2);
+        }
+
+        if ($this->input->post('option_name3') != NULL) {
+            $item_option3 = array(
+                'item_id' => $id,
+                'option_name' => $this->input->post('option_name3'),
+                'price' => $this->input->post('price3')
+            );
+            $this->db->insert("ITEM_OPTIONS", $item_option3);
+        }
     }
 
 }
